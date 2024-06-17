692f9de4104d9ee87b44d0f9919d6a7cf89376d4
rutu-sh <ruturajs10@hotmail.com>
2024-06-17 09:27:45 -0400

    feat: initial configuration tools

---
+++ cloudlab_config.mk
@@ -0,0 +1,19 @@
+# CloudLab configurations for Development Ease 
+
+# Local ssh-key path
+SSH_KEY_PATH=~/gwu/gwu-cloud-lab
+
+# User Configurations
+# Your Cloudlab Username
+CLOUDLAB_USERNAME=rutu_sh
+
+# Node Configurations
+# Paste the configuration of your nodes here
+# Node 0
+NODE_0=c220g2-010606.wisc.cloudlab.us
+
+# Node 1
+NODE_1=c220g2-010608.wisc.cloudlab.us
+
+# Node 2
+NODE_2=c220g2-010604.wisc.cloudlab.us
